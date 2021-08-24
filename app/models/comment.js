8c715fccbd67fed2729974b832d1774132feb114
naguiluz <nicolasaguiluz2@gmail.com>
2021-08-24 13:10:35 -0500

    Added curlscripts and routes + update server.js

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,25 +3,25 @@
 const mongoose = require('mongoose')
 // eslint-disable-next-line no-unused-vars
 const User = require('./user')
-// const Schema = mongoose.Schema
+const Schema = mongoose.Schema
 
 const commentSchema = new mongoose.Schema(
-	{
-		text: {
-			type: String,
-			required: true,
-		},
+  {
+    text: {
+      type: String,
+      required: true
+    },
     image: {
       type: String
     },
-		owner: {
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	},
-	{
-		timestamps: true,
-	}
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
 )
 // do not need to create model for a sub-document
 module.exports = commentSchema
