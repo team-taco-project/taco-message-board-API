85064c6debe2dce3d7878692aebcd6719aa6d506
tharitar8 <86535497+tharitar8@users.noreply.github.com>
2021-08-24 13:38:00 -0400

    comments

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,18 +3,25 @@
 const mongoose = require('mongoose')
 // eslint-disable-next-line no-unused-vars
 const User = require('./user')
+// const Schema = mongoose.Schema
 
-const Schema = mongoose.Schema
-
-const commentSchema = new Schema({
-  body: String,
-  title: String,
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  }
-}, {
-  timestamps: true
-})
+const commentSchema = new mongoose.Schema(
+	{
+		text: {
+			type: String,
+			required: true,
+		},
+    image: {
+      type: String
+    },
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
 // do not need to create model for a sub-document
 module.exports = commentSchema
