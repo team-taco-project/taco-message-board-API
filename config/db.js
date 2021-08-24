20f186f9a3c050f41b6c423c7825ae10563324f5
naguiluz <nicolasaguiluz2@gmail.com>
2021-08-24 09:54:33 -0500

    Added models and updated installs/names

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'taco-message-board-API'
 
 // create the mongodb uri for development and test
 const database = {
