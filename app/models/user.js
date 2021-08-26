f9b8f698c0a25ed4a15c44308058834a03bae0aa
naguiluz <nicolasaguiluz2@gmail.com>
2021-08-26 18:36:10 -0500

    update comment almost there just finding id wrong

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose')
+const commentSchema = require('./comment')
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    hashedPassword: {
+      type: String,
+      required: true
+    },
+    comments: [commentSchema],
+    token: String
   },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  token: String
-}, {
-  timestamps: true,
-  toObject: {
-    // remove `hashedPassword` field when we call `.toObject`
-    transform: (_doc, user) => {
-      delete user.hashedPassword
-      return user
+  {
+    timestamps: true,
+    toObject: {
+      // remove `hashedPassword` field when we call `.toObject`
+      transform: (_doc, user) => {
+        delete user.hashedPassword
+        return user
+      }
     }
   }
-})
+)
 
 module.exports = mongoose.model('User', userSchema)
--- app/routes/comment_routes.js
+++ app/routes/comment_routes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 // require post model
 const Post = require('./../models/post')
 const customErrors = require('../../lib/custom_errors')
+// const passport = require('passport')
+// const requireToken = passport.authenticate('bearer', { session: false })
 
 // we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
@@ -46,19 +48,36 @@ router.delete('/post/:postId/:commentId', (req, res, next) => {
 
 // UPDATE
 // PATCH /comments/:id
-router.patch('/update-comments/:id', (req, res, next) => {
-  const commentId = req.params.id
+router.patch('/post/:commentId', (req, res, next) => {
+  const commentId = req.params.commentId
   const commentData = req.body.comment
-  Post.findOne({
-    'comments._id': commentId
-  })
+  Post.findById(commentId)
     .then(handle404)
-    .then((post) => {
-      const comment = post.comments.id(commentId)
-      comment.set(commentData)
-      return post.save()
+    .then((comment) => {
+      const newComment = comment.id(commentId)
+      newComment.set(commentData)
+      return comment.save()
     })
     .then(() => res.sendStatus(204))
     .catch(next)
 })
+
+// // SHOW
+// // GET /posts/5a7db6c74d55bc51bdf39793
+// router.get('/post/:postId/:commentId', requireToken, (req, res, next) => {
+//   // req.params.id will be set based on the `:id` in the route
+//   const postId = req.params.postId
+//   const commentId = req.params.commentId
+//   Post.findById(postId)
+//     .then(handle404)
+//   // if `findById` is succesful, respond with 200 and "post" JSON
+//   // find comment
+//   // return comment
+//     .then((post) => {
+//       const comment = post.comments.id(commentId)
+//       res.status(200).json({ comments: comment.toObject() })
+//     })
+//   // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 module.exports = router
